df069062230b0344e0979fbeab0318c536b2d16d
LucyRa <40637257+LucyRa@users.noreply.github.com>
2019-02-16 20:18:40 +0000

    Creating second version templates, layouts, styling and nav with js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,18 +2,16 @@ window.onscroll= function(){
     scroll();
 }
 
-var page = document.getElementById('services');
-
 var nav = document.getElementById('navbar');
 
 var stick = page.offsetTop;
 
 function scroll(){
-    if (window.pageYOffset >= 760){
+    if (window.pageYOffset >= 100){
         nav.classList.add('pagenav')
         
     } else {
-        nav.classList.remove('pagenav');
+        nav.classList.remove('pagenav').style.transition = "0.5s";
         
     }
 }
